b5cb9973b3d3640972d6926032b7e68cc2de1d9b
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-06-13 09:00:31 -0500

    First commit

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='0.1.1',
     packages=['pyblip'],
     url='https://github.com/mminichino/python-blip',
-    license='MIT License',
+    license='Apache License 2.0',
     author='Michael Minichino',
     python_requires='>=3.9',
     scripts=['bin/blipctl'],
@@ -25,5 +25,17 @@ setup(
     author_email='info@unix.us.com',
     description='Couchbase BLIP Protocol Library',
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    keywords=["couchbase", "blip", "mobile", "syncgateway"],
+    classifiers=[
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: Apache Software License",
+          "Intended Audience :: Developers",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Topic :: Database",
+          "Topic :: System :: Networking",
+          "Topic :: Software Development :: Libraries",
+          "Topic :: Software Development :: Libraries :: Python Modules"],
 )
